e9eaced7907ec870a10f6e46e4e3541311808b8a
Paulo Diego <pdiegodo@gmail.com>
2024-08-05 23:39:30 -0300

    Testar conexão com o banco de dados

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+
+namespace TestConnection
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string connectionString = "Server=192.168.0.75\\SQLEXPRESS;Database=LivroVault;Integrated Security=True;TrustServerCertificate=True;";
+
+            using SqlConnection connection = new(connectionString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Conexão bem-sucedida!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao conectar: " + ex.Message);
+            }
+        }
+    }
+}
